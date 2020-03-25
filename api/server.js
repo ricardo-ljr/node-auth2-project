acb091bee1db143ddc4e064d2af9d697fd7c3165
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-03-25 14:36:26 -0600

    Routers Added XD

--- api/server.js
+++ api/server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../users/users-router");
+const restricted = require("../auth/restricted-middleware");
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, usersRouter);
 
 server.get("/", (req, res) => {
   res.send({ api: "up" });
